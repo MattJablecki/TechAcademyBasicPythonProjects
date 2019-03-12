feb3c0c3a82ffd42276be23509eadc2bbec034c6
MattJablecki <46763228+MattJablecki@users.noreply.github.com>
2019-03-12 13:03:05 -0700

    Add files via upload

---
+++ Test/FileIO.py
@@ -0,0 +1,24 @@
+
+import os
+
+
+def get_filez ():
+    direct = os.getcwd()
+    for file in os.listdir(direct):
+        time = os.path.getmtime(file)
+        abPath=os.path.join(direct,file)
+        if file.endswith(".txt"):
+            print (abPath,time)
+        
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    get_filez()
+
+
